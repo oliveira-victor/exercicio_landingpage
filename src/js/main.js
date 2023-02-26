b19170873215cd6f90d35a4cbef0e0ab6f3f49f7
victor <victor.freire.oliveira@gmail.com>
2023-02-26 12:41:10 -0300

    efeito js no header

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeight) {
+            headerGradient()
+        } else {
+            headerSolid()
+        }
+    })
+
+    function headerGradient() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function headerSolid() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+
+    // Seção da trilogia, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
